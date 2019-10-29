9d5185c9d24f6b12fecf72cdbd86f40d34dd32b8
Ale Bausa <ale.bausa@gmail.com>
2019-10-29 14:29:23 +0100

    fixes login

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import React, { Component } from 'react';
 import { withAuth } from '../../Context/AuthContext';
 import authservice from '../../services/authService';
 
