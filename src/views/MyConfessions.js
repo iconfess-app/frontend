8a73762325a04e8fb5689402672019a8521ef9a0
Nomeolvides <info@nomeolvides.cat>
2019-11-17 20:43:41 +0100

    Closes #61-loading-component

--- src/views/MyConfessions.js
+++ src/views/MyConfessions.js
@@ -4,6 +4,7 @@ import { withFlash } from '../Context/NotificationContext';
 import confessionService from '../services/confessionService';
 import CardMyConfessions from './components/CardMyConfession';
 import NavBar from './components/NavBar';
+import Loading from './components/Loading';
 
 class myConfessionsPage extends Component {
   constructor(props) {
@@ -76,38 +77,44 @@ class myConfessionsPage extends Component {
     const { loading } = this.state;
     const goBack = () => this.props.history.goBack();
     return (
-      <div className="container">
-        <div className="content">
-          <div className="top-header">
-            <span onClick={goBack}>
-              <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M13.2624 19.1765L4.36879 10.812L13.2624 2.29805L11.078 0.206909L0 10.812L11.078 21.2677L13.2624 19.1765Z"
-                  fill="white"
-                  fillOpacity="0.6"
-                />
-              </svg>
-            </span>
-            <h3>My confessions</h3>
-            <svg
-              width="22"
-              height="21"
-              viewBox="0 0 22 21"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              onClick={this.handleEncoded}
-            >
-              <path
-                d="M19.3 0L7.8 11.5C7 11 6 10.7 5 10.7C2.2 10.7 0 12.9 0 15.7C0 18.5 2.2 20.7 5 20.7C7.8 20.7 10 18.5 10 15.7C10 14.7 9.7 13.7 9.2 12.9L15 7.1L17.3 9.4L18.7 8L16.4 5.7L18 4.1L20.3 6.4L21.7 5L19.4 2.7L20.7 1.4L19.3 0ZM5 18.7C3.3 18.7 2 17.4 2 15.7C2 14 3.3 12.7 5 12.7C6.7 12.7 8 14 8 15.7C8 17.4 6.7 18.7 5 18.7Z"
-                fill="white"
-                fillOpacity="0.6"
-              />
-            </svg>
+      <>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="container">
+            <div className="content">
+              <div className="top-header">
+                <span onClick={goBack}>
+                  <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M13.2624 19.1765L4.36879 10.812L13.2624 2.29805L11.078 0.206909L0 10.812L11.078 21.2677L13.2624 19.1765Z"
+                      fill="white"
+                      fillOpacity="0.6"
+                    />
+                  </svg>
+                </span>
+                <h3>My confessions</h3>
+                <svg
+                  width="22"
+                  height="21"
+                  viewBox="0 0 22 21"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={this.handleEncoded}
+                >
+                  <path
+                    d="M19.3 0L7.8 11.5C7 11 6 10.7 5 10.7C2.2 10.7 0 12.9 0 15.7C0 18.5 2.2 20.7 5 20.7C7.8 20.7 10 18.5 10 15.7C10 14.7 9.7 13.7 9.2 12.9L15 7.1L17.3 9.4L18.7 8L16.4 5.7L18 4.1L20.3 6.4L21.7 5L19.4 2.7L20.7 1.4L19.3 0ZM5 18.7C3.3 18.7 2 17.4 2 15.7C2 14 3.3 12.7 5 12.7C6.7 12.7 8 14 8 15.7C8 17.4 6.7 18.7 5 18.7Z"
+                    fill="white"
+                    fillOpacity="0.6"
+                  />
+                </svg>
+              </div>
+              <div className="confessions">{this.renderMyConfessions()}</div>
+            </div>
+            <NavBar />
           </div>
-          {loading ? 'loading...' : this.renderMyConfessions()}
-        </div>
-        <NavBar />
-      </div>
+        )}
+      </>
     );
   }
 }
