23d2aaf333448a59a0aa800c63081eda3e78da14
Nomeolvides <info@nomeolvides.cat>
2019-11-19 10:20:10 +0100

    Closes #92-notifications-chat-css

--- src/views/components/NavBar.js
+++ src/views/components/NavBar.js
@@ -1,65 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { withAuth } from '../../Context/AuthContext';
 
-const NavBar = () => {
-  return (
-    <nav>
-      <div className="navbar">
-        <NavLink to="/" exact activeClassName="navbar__active" className="navbar__item">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M1.4 14.1L2.7 12.8V22.4H20.7V12.8L22 14.1L23.4 12.7L11.7 1L6.7 6V4.4H4.7V8L0 12.7L1.4 14.1ZM18.7 10.8V20.4H4.7V10.8L11.7 3.8L18.7 10.8Z"
-              fill="white"
-            />
-          </svg>
-          <span className="navbar__label">Home</span>
-        </NavLink>
-        <NavLink to="/privacy-policy" exact activeClassName="navbar__active" className="navbar__item">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 22H18V20H16V9H7V11H9V22ZM11 11H14V20H11V11Z" fill="white" />
-            <path
-              d="M12.5 8C14.4 8 16 6.4 16 4.5C16 2.6 14.4 1 12.5 1C10.6 1 9 2.6 9 4.5C9 6.4 10.6 8 12.5 8ZM12.5 3C13.3 3 14 3.7 14 4.5C14 5.3 13.3 6 12.5 6C11.7 6 11 5.3 11 4.5C11 3.7 11.7 3 12.5 3Z"
-              fill="white"
-            />
-          </svg>
-          <span className="navbar__label">Policy</span>
-        </NavLink>
-        <NavLink to="/confess" exact className="add-btn navbar__item">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M10.9589 24H13.1507V13.1507H24V10.9589H13.1507V0H10.9589V10.9589H0V13.1507H10.9589V24Z"
-              fill="white"
-            />
-          </svg>
-        </NavLink>
-        <NavLink to="/myconfessions" exact activeClassName="navbar__active" className="navbar__item">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M1 22H23V8H24V4C24 2.3 22.7 1 21 1H3C1.3 1 0 2.3 0 4V8H1V22ZM21 20H3V8H21V20ZM2 4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6H2V4Z"
-              fill="white"
-            />
-            <path d="M18 10H6V12H18V10Z" fill="white" />
-          </svg>
-          <span className="navbar__label">Archive</span>
-        </NavLink>
-        <NavLink to="/edit" exact activeClassName="navbar__active" className="navbar__item">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M22 19.8C22 15.51 18.59 12.1 14.3 12.1H7.7C3.41 12.1 0 15.51 0 19.8V22H22V19.8ZM2.2 19.8C2.2 16.72 4.62 14.3 7.7 14.3H14.3C17.38 14.3 19.8 16.72 19.8 19.8H2.2Z"
-              fill="white"
-            />
-            <path
-              d="M16.5 5.5C16.5 2.42 14.08 0 11 0C7.91997 0 5.49997 2.42 5.49997 5.5C5.49997 8.58 7.91997 11 11 11C14.08 11 16.5 8.58 16.5 5.5ZM7.69997 5.5C7.69997 3.63 9.12997 2.2 11 2.2C12.87 2.2 14.3 3.63 14.3 5.5C14.3 7.37 12.87 8.8 11 8.8C9.12997 8.8 7.69997 7.37 7.69997 5.5Z"
-              fill="white"
-            />
-          </svg>
-          <span className="navbar__label">Profile</span>
-        </NavLink>
-      </div>
-    </nav>
-  );
-};
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAllowsContact: '',
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      userAllowsContact: this.props.user.allowsContact,
+    });
+  };
+
+  render() {
+    const { userAllowsContact } = this.state;
+    return (
+      <nav>
+        <div className="navbar">
+          <NavLink to="/" exact activeClassName="navbar__active" className="navbar__item">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1.4 14.1L2.7 12.8V22.4H20.7V12.8L22 14.1L23.4 12.7L11.7 1L6.7 6V4.4H4.7V8L0 12.7L1.4 14.1ZM18.7 10.8V20.4H4.7V10.8L11.7 3.8L18.7 10.8Z"
+                fill="white"
+              />
+            </svg>
+            <span className="navbar__label">Home</span>
+          </NavLink>
+          {userAllowsContact ? (
+            <NavLink to="/chat" exact activeClassName="navbar__active" className="navbar__item">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M19.3554 2H4.47934C3.07438 2 2 3.07438 2 4.47934V15.2231C2 16.6281 3.07438 17.7025 4.47934 17.7025H5.30579V22L10.1818 17.7025H19.3554C20.7603 17.7025 21.8347 16.6281 21.8347 15.2231V4.47934C21.8347 3.07438 20.7603 2 19.3554 2ZM20.1818 15.2231C20.1818 15.719 19.8512 16.0496 19.3554 16.0496H9.52066L6.95868 18.3636V16.0496H4.47934C3.98347 16.0496 3.65289 15.719 3.65289 15.2231V4.47934C3.65289 3.98347 3.98347 3.65289 4.47934 3.65289H19.3554C19.8512 3.65289 20.1818 3.98347 20.1818 4.47934V15.2231Z"
+                  fill="white"
+                />
+                <path d="M17.7025 6.95868H6.13223V8.61157H17.7025V6.95868Z" fill="white" />
+                <path d="M17.7025 10.2645H6.13223V11.9174H17.7025V10.2645Z" fill="white" />
+              </svg>
+              <span className="navbar__label">Chat</span>
+            </NavLink>
+          ) : (
+            <NavLink to="/privacy-policy" exact activeClassName="navbar__active" className="navbar__item">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 22H18V20H16V9H7V11H9V22ZM11 11H14V20H11V11Z" fill="white" />
+                <path
+                  d="M12.5 8C14.4 8 16 6.4 16 4.5C16 2.6 14.4 1 12.5 1C10.6 1 9 2.6 9 4.5C9 6.4 10.6 8 12.5 8ZM12.5 3C13.3 3 14 3.7 14 4.5C14 5.3 13.3 6 12.5 6C11.7 6 11 5.3 11 4.5C11 3.7 11.7 3 12.5 3Z"
+                  fill="white"
+                />
+              </svg>
+              <span className="navbar__label">Policy</span>
+            </NavLink>
+          )}
+          <NavLink to="/confess" exact className="add-btn navbar__item">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M10.9589 24H13.1507V13.1507H24V10.9589H13.1507V0H10.9589V10.9589H0V13.1507H10.9589V24Z"
+                fill="white"
+              />
+            </svg>
+          </NavLink>
+          <NavLink to="/myconfessions" exact activeClassName="navbar__active" className="navbar__item">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1 22H23V8H24V4C24 2.3 22.7 1 21 1H3C1.3 1 0 2.3 0 4V8H1V22ZM21 20H3V8H21V20ZM2 4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6H2V4Z"
+                fill="white"
+              />
+              <path d="M18 10H6V12H18V10Z" fill="white" />
+            </svg>
+            <span className="navbar__label">Archive</span>
+          </NavLink>
+          <NavLink to="/edit" exact activeClassName="navbar__active" className="navbar__item">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M22 19.8C22 15.51 18.59 12.1 14.3 12.1H7.7C3.41 12.1 0 15.51 0 19.8V22H22V19.8ZM2.2 19.8C2.2 16.72 4.62 14.3 7.7 14.3H14.3C17.38 14.3 19.8 16.72 19.8 19.8H2.2Z"
+                fill="white"
+              />
+              <path
+                d="M16.5 5.5C16.5 2.42 14.08 0 11 0C7.91997 0 5.49997 2.42 5.49997 5.5C5.49997 8.58 7.91997 11 11 11C14.08 11 16.5 8.58 16.5 5.5ZM7.69997 5.5C7.69997 3.63 9.12997 2.2 11 2.2C12.87 2.2 14.3 3.63 14.3 5.5C14.3 7.37 12.87 8.8 11 8.8C9.12997 8.8 7.69997 7.37 7.69997 5.5Z"
+                fill="white"
+              />
+            </svg>
+            <span className="navbar__label">Profile</span>
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default withAuth(NavBar);
 // this.props.user.allowsConnection === true > chat / false === policy information
--- src/views/components/Notification.js
+++ src/views/components/Notification.js
@@ -1,43 +1,51 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Flash extends Component {
   state = {
     mounted: true,
-  }
+  };
 
   componentDidMount = () => {
     setTimeout(() => {
       this.setState({
         mounted: false,
-      })
+      });
     }, 5000);
-  }
+  };
 
   handleClose = () => {
-    this.setState({ mounted: false })
-  }
+    this.setState({ mounted: false });
+  };
 
   render() {
     const { type, message } = this.props;
     const { mounted } = this.state;
     return (
-      <div className={`notification ${mounted ? 'slide-in' : 'slide-out'} ${type}`}> {message}<img className='close_notification' onClick={this.handleClose} alt='cross-icon' src='images/icon-close-black.png' /></div >
-    )
+      <div className={`notification ${mounted ? 'slide-in' : 'slide-out'} ${type}`}>
+        {' '}
+        {message}
+        {/* <img
+          className="close_notification"
+          onClick={this.handleClose}
+          alt="cross-icon"
+          src="images/icon-close-black.png"
+        /> */}
+        <svg
+          width="17"
+          height="16"
+          viewBox="0 0 17 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="close_notification"
+          onClick={this.handleClose}
+          alt="cross-icon"
+        >
+          <path d="M2.01342 15.6167L8.6623 9.38334L15.3112 15.6167L16.6801 14.3333L10.0312 8.1L16.6801 1.86667L15.3112 0.583336L8.6623 6.81667L2.01342 0.583336L0.644531 1.86667L7.29341 8.1L0.644531 14.3333L2.01342 15.6167Z" />
+        </svg>
+      </div>
+    );
   }
 }
 
 export default Flash;
-
-
-
-
-
-
-
-
-
-
-
-
-
